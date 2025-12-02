d6ade702420102058ffca965169019c5d14c7c4b
Elliot <elliots1m@hotmail.co.uk>
2025-12-02 09:14:34 +0000

    AOC first 2 days

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Day1;
+
+string[] lines = Input.INPUT.Split("\r\n");
+
+const int start = 50;
+int pos = start;
+int prev = pos;
+int count = 0;
+int pt2count = 0; ;
+foreach (var line in lines)
+{
+    int number = int.Parse(line.Substring(1));
+    
+    if (line[0] == 'R')
+    {
+        pos += number;
+        while (pos > 99)
+        {
+            pos -= 100;
+            pt2count++;
+            
+        }
+    }
+    else if (line[0] == 'L')
+    {
+        pos -= number;
+        while (pos < 0)
+        {
+            pos = 100 + pos;
+            if (prev != 0)
+            {
+                pt2count++;
+            }
+            else
+            {
+                prev = 1; 
+            }
+        }
+    }
+    else
+    {
+        continue;
+    }
+
+    if (pos == 0)
+    {
+        count++;
+        if (line[0] == 'L')
+        {
+            pt2count++;
+        }
+    }
+    prev = pos;
+}
+
+Console.WriteLine(count.ToString());
+Console.WriteLine(pt2count.ToString());
---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿using Day2;
+
+string[] IdPairs = Input.INPUT.Split(',');
+ulong count = 0;
+ulong count2 = 0;
+foreach (string idPair in IdPairs)
+{
+    string[] idPairsSplit = idPair.Split("-");
+    string id1 = idPairsSplit[0];
+    string id2 = idPairsSplit[1];
+    bool id1OneLength = id1.Length / 2 == 0;
+    bool id2OddLength = id2.Length % 2 == 1;
+    /*if (id1.Length % 2 == 1 && id1.Length == id2.Length)
+    {
+        continue;
+    }
+    else*/
+    {
+        ulong id1FirstHalf; 
+        if (id1OneLength)
+        {
+            id1FirstHalf = 1;
+        }
+        else
+        {
+            id1FirstHalf = ulong.Parse(id1.Substring(0, id1.Length / 2));
+        }
+        ulong id2FirstHalf = ulong.Parse(id2.Substring(0, id2.Length / 2 + (id2OddLength ? 1 : 0)));
+
+        ulong intId1 = ulong.Parse(id1);
+        ulong intId2 = ulong.Parse(id2);
+
+        for (ulong i = id1FirstHalf; i <= id2FirstHalf; i++)
+        {
+            ulong doubleI = ulong.Parse(i.ToString() + i.ToString());
+            if (doubleI >= intId1 && doubleI <= intId2)
+            {
+                count += (ulong)doubleI;
+            }
+            else if (doubleI > intId2)
+            {
+                break;
+            }
+
+        }
+
+        HashSet<ulong> list = new HashSet<ulong>();
+        for (ulong i = 1; i <= id2FirstHalf; i++) 
+        {
+            string stringI = i.ToString();
+            while (stringI.Length <= id2.Length)
+            {
+                ulong concatI = ulong.Parse(stringI);
+                if (concatI >= intId1 && concatI <= intId2)
+                {
+                    if (!list.Contains(concatI))
+                    {
+                        count2 += concatI;
+                        list.Add(concatI);
+                    }
+                }
+                stringI += i.ToString();
+            }
+        }
+    }
+}
+
+Console.WriteLine(count);
+Console.WriteLine(count2);
