32d42cc3a8f916d5b7dd11f816de93dd14950d45
Elliot <elliots1m@hotmail.co.uk>
2025-12-05 08:14:24 +0000

    Days 4 and 5

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,84 @@
+﻿using Day4;
+
+string[] lines = Input.INPUT.Split("\r\n");
+char[,] rollMap = new char[lines[0].Length, lines.Length];
+
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[0].Length; x++)
+    {
+        rollMap[x,y] = lines[y][x];
+    }
+}
+
+var BoundedCheck = (int x, int y) => { 
+    if (x < 0 || y < 0 || x >= lines[0].Length || y >= lines.Length || rollMap[x, y] == '.') { return 0; }
+    else { return 1; } 
+};
+
+int total = 0;
+
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[0].Length; x++)
+    {
+        int currentCount = 0;
+        if (rollMap[x, y] == '@')
+        {
+            for (int up = -1; up <= 1; up++)
+            {
+                for (int right = -1; right <= 1; right++)
+                {
+                    if (!(right == 0 && up == 0))
+                    {
+                        currentCount += BoundedCheck(x + right, y + up);
+                        if (currentCount >= 4)
+                        {
+                            break;
+                        }
+                    }
+
+                }
+                if (currentCount >= 4) { break; }
+            }
+            if (currentCount < 4) { total++; }
+        }
+    }
+}
+
+int lastTotal = 0;
+int totalpt2 = 0;
+do
+{
+    lastTotal = totalpt2;
+    for (int y = 0; y < lines.Length; y++)
+    {
+        for (int x = 0; x < lines[0].Length; x++)
+        {
+            int currentCount = 0;
+            if (rollMap[x, y] == '@')
+            {
+                for (int up = -1; up <= 1; up++)
+                {
+                    for (int right = -1; right <= 1; right++)
+                    {
+                        if (!(right == 0 && up == 0))
+                        {
+                            currentCount += BoundedCheck(x + right, y + up);
+                            if (currentCount >= 4)
+                            {
+                                break;
+                            }
+                        }
+
+                    }
+                    if (currentCount >= 4) { break; }
+                }
+                if (currentCount < 4) { totalpt2++; rollMap[x, y] = '.'; }
+            }
+        }
+    }
+} while (lastTotal != totalpt2);
+
+Console.WriteLine(total);
+Console.WriteLine(totalpt2);
---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Specialized;
+using Day5;
+
+string[] lines = Input.INPUT.Split("\r\n");
+SortedDictionary<ulong, ulong> valid = new();
+bool ranges = true;
+ulong count = 0;
+foreach (string line in lines)
+{
+    if (line == "")
+    {
+        ranges = false;
+        continue;
+    }
+
+    if (ranges)
+    {
+        string[] ends = line.Split('-');
+        ulong start = ulong.Parse(ends[0]);
+        ulong end = ulong.Parse(ends[1]);
+
+        var overlapping_starts = valid.Where(x => x.Key >= start && x.Key <= end);
+        bool added = false;
+
+        if (overlapping_starts.Any())
+        {
+            ulong max_end = overlapping_starts.Last().Value;
+            foreach (var entry in overlapping_starts.ToList())
+            {
+                valid.Remove(entry.Key);
+            }
+            if (end >= max_end)
+            {
+                max_end = end;
+            }
+            valid.Add(start, max_end);
+            end = max_end;
+            added = true;
+        }
+
+        var overlapping_ends = valid.Where(x => x.Value >= start && x.Value <= end);
+        if (overlapping_ends.Any())
+        {
+            ulong min_start = overlapping_ends.First().Key;
+            foreach (var entry in overlapping_ends.ToList())
+            {
+                valid.Remove(entry.Key);
+            }
+            if (start <= min_start)
+            {
+                min_start = start;
+            }
+            valid.Add(min_start, end);
+            added = true;
+        }
+
+        if (!added && !valid.Where(x => x.Key <= start && x.Value >= end).Any())
+        {
+            valid.Add(start, end);
+        }
+    }
+    else
+    {
+        ulong value = ulong.Parse(line);
+        if (valid.Where(x => x.Key <= value && x.Value >= value).Any())
+        {
+            count++;
+        }
+    }
+}
+ulong total_valid = 0;
+foreach (var entry in valid)
+{
+    total_valid += entry.Value - entry.Key + 1;
+}
+
+Console.WriteLine(count);
+Console.WriteLine(total_valid);
