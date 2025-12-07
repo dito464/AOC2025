18ab2f1cbbc20fb45d29e7a4465853e75c3cdec2
Elliot <elliots1m@hotmail.co.uk>
2025-12-07 20:53:15 +0000

    Weekend AOC

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,92 @@
+﻿using Day6;
+
+string[] lines = Input.INPUT.Split("\r\n");
+string[] lengthTracker = lines[0].Split(' ').Where(x => x != "").ToArray();
+string[,] vals = new string[lengthTracker.Length, lines.Length];
+
+for (int y = 0; y < lines.Length; y++)
+{
+    string[] line = lines[y].Split(' ').Where(x => x != "").ToArray();
+    for (int x = 0; x < line.Length; x++)
+    {
+        vals[x, y] = line[x];
+    }
+}
+
+char[,] valspt2 = new char[lines[0].Length, lines.Length];
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[0].Length; x++)
+    {
+        valspt2[x, y] = lines[y][x];
+    }
+}
+
+string[,] realValspt2 = new string[lengthTracker.Length, lines[0].Length + 1];
+int realyIndex = 0;
+int realxIndex = 0;
+for (int x = 0; x < lines[0].Length; x++)
+{
+    string currentNum = "";
+
+    for (int y = 0; y < lines.Length - 1; y++)
+    {
+        currentNum += lines[y][x];
+    }
+    long value;
+    if (!long.TryParse(currentNum, out value))
+    {
+        realyIndex = 0;
+        realxIndex++;
+        continue;
+    }
+    realValspt2[realxIndex, realyIndex++] = currentNum;
+}
+
+int i = 0;
+for (int x = 0; x < lines[0].Length; x++)
+{
+    if (valspt2[x, lines.Length - 1] == '*' || valspt2[x, lines.Length - 1] == '+')
+    {
+        realValspt2[i++, lines[0].Length] = valspt2[x, lines.Length - 1].ToString();
+    }
+}
+
+var DoMath = (string[,] values) =>
+{
+    long total = 0;
+    for (int x = 0; x < values.GetLength(0); x++)
+    {
+        string operation = values[x, values.GetLength(1) - 1];
+        long subtotal = 0;
+        if (operation == "*")
+        {
+            subtotal = 1;
+        }
+        for (int y = 0; y < values.GetLength(1) - 1; y++)
+        {
+            if (values[x, y] == null)
+            {
+                continue;
+            }
+            long currentVal = long.Parse(values[x, y]);
+            if (currentVal == -1)
+            {
+                continue;
+            }
+            if (operation == "*")
+            {
+                subtotal *= currentVal;
+            }
+            else if (operation == "+")
+            {
+                subtotal += currentVal;
+            }
+        }
+        total += subtotal;
+    }
+    return total;
+};
+
+Console.WriteLine(DoMath(vals));
+Console.WriteLine(DoMath(realValspt2));
---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,85 @@
+﻿using Day7;
+
+string[] lines = Input.INPUT.Split("\r\n");
+char[,] manifold = new char[lines[0].Length, lines.Length];
+(int x, int y) start = (0, 0);
+char startChar = 'S';
+char splitterChar = '^';
+char emptyChar = '.';
+
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[y].Length; x++)
+    {
+        manifold[x, y] = lines[y][x];
+        if (lines[y][x] == startChar)
+        {
+            start = (x, y);
+        }
+    }
+}
+
+HashSet<int> lastRow = new HashSet<int>() { start.x };
+int count = 0;
+
+for (int y = start.y + 1; y < lines.Length; y++)
+{
+    HashSet<int> nextRow = new HashSet<int>();
+    foreach (int column in lastRow)
+    {
+        if (manifold[column, y] == splitterChar)
+        {
+            count++;
+            if (column > 0)
+            {
+                nextRow.Add(column - 1);
+            }
+            if (column < lines[0].Length - 1)
+            {
+                nextRow.Add(column + 1);
+            }
+        }
+        else if (manifold[column, y] == emptyChar)
+        {
+            nextRow.Add(column);
+        }
+    }
+    lastRow = nextRow;
+}
+
+Dictionary<(int, int), ulong> cache = new();
+ulong totalRoutes(int column, int row)
+{
+    if (row == lines.Length - 1)
+    {
+        return 1;
+    }
+
+    if (manifold[column, row + 1] == splitterChar)
+    {
+        ulong localCount = 0;
+        if (cache.TryGetValue((column, row + 1), out localCount))
+        {
+            return localCount;
+        }
+
+        if (column > 0)
+        {
+            localCount += totalRoutes(column - 1, row + 1);
+        }
+        if (column < lines[0].Length - 1)
+        {
+            localCount += totalRoutes(column + 1, row + 1);
+        }
+        cache[(column, row + 1)] = localCount;
+        return localCount;
+    }
+    else if (manifold[column, row + 1] == emptyChar)
+    {
+        return totalRoutes(column, row + 1);
+    }
+    return 1;
+}
+
+Console.WriteLine(count);
+Console.WriteLine(totalRoutes(start.x, start.y));
