b73ee4c4ba939a18277358ed9fc9dc56235e3655
Elliot <elliots1m@hotmail.co.uk>
2025-12-09 17:54:40 +0000

    days 8 and 9

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Data;
+using System.Security.Cryptography;
+using Day8;
+
+string[] lines = Input.INPUT.Split("\r\n");
+
+SortedDictionary<double, (Coordinate, Coordinate)> sizes = new();
+Dictionary<Coordinate, HashSet<Coordinate>> connections = new();
+
+Coordinate[] coordinates = new Coordinate[lines.Length];
+HashSet<Coordinate> seenAdded = new();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    string[] coords = lines[i].Split(',');
+    Coordinate coordinate = new Coordinate(
+        int.Parse(coords[0]),
+        int.Parse(coords[1]),
+        int.Parse(coords[2])
+    );
+    coordinates[i] = coordinate;
+}
+
+var straightLineDistance = (Coordinate c1, Coordinate c2) =>
+{
+    long x = c1.x - c2.x;
+    long y = c1.y - c2.y;
+    long z = c1.z - c2.z;
+    return Math.Sqrt(x * x + y * y + z * z);
+};
+
+for (int i = 0; i < coordinates.Length - 1; i++)
+{
+    for (int j = i + 1; j < coordinates.Length; j++)
+    {
+        sizes.Add(
+            straightLineDistance(coordinates[i], coordinates[j]),
+            (coordinates[i], coordinates[j])
+        );
+    }
+}
+
+int connectionCount = 0;
+const int maxConnections = 1000;
+
+foreach (var kvp in sizes)
+{
+    (Coordinate c1, Coordinate c2) = kvp.Value;
+    HashSet<Coordinate> possibleC1s = null;
+    HashSet<Coordinate> possibleC2s = null;
+    connections.TryGetValue(c1, out possibleC1s);
+    connections.TryGetValue(c2, out possibleC2s);
+
+    if (possibleC1s == null && possibleC2s == null)
+    {
+        HashSet<Coordinate> set = new HashSet<Coordinate>() { c1, c2 };
+        connections.Add(c1, set);
+        connections.Add(c2, set);
+    }
+    else if (possibleC1s == null && possibleC2s != null)
+    {
+        possibleC2s.Add(c1);
+        connections.Add(c1, possibleC2s);
+    }
+    else if (possibleC1s != null && possibleC2s == null)
+    {
+        possibleC1s.Add(c2);
+        connections.Add(c2, possibleC1s);
+    }
+    else if (!possibleC1s.Contains(c2) && !possibleC2s.Contains(c1))
+    {
+        foreach (var connection in possibleC1s)
+        {
+            possibleC2s.Add(connection);
+        }
+        foreach (var connection in possibleC1s)
+        {
+            connections[connection] = possibleC2s;
+        }
+    }
+    /*else
+    {
+        continue;
+    }*/
+
+    connectionCount++;
+    if (connectionCount == maxConnections)
+    {
+        int result = 1;
+        int groupCount = 0;
+        const int largestGroups = 3;
+        HashSet<Coordinate> seenRemoved = new();
+        while (connections.Count > 0)
+        {
+            var kvp2 = connections.MaxBy(x => x.Value.Count);
+            result *= kvp2.Value.Count;
+            foreach (var connection in kvp2.Value)
+            {
+                if (seenRemoved.Contains(connection))
+                {
+                    Console.WriteLine(
+                        $"Duplicate Coord {connection.x},{connection.y},{connection.z}"
+                    );
+                }
+                else
+                {
+                    seenRemoved.Add(connection);
+                }
+                connections.Remove(connection);
+            }
+            groupCount++;
+            if (groupCount == largestGroups)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+    if (possibleC2s != null && possibleC2s.Count == lines.Length)
+    {
+        Console.WriteLine((long)c1.x * (long)c2.x);
+        break;
+    }
+}
---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Numerics;
+using Day9;
+
+string[] lines = Input.INPUT.Split("\r\n");
+SortedDictionary<long, List<long>> xToY = new();
+SortedDictionary<long, List<long>> yToX = new();
+(long x, long y)[] corners = new (long, long)[lines.Length];
+Dictionary<(long, long), long> cornerMap = new();
+SortedDictionary<long, List<long>> xToYEdges = new();
+SortedDictionary<long, List<long>> yToXEdges = new();
+
+long maxAreaPt1 = 0;
+long maxAreaPt2 = 0;
+var wrappedNext = (long val) =>
+{
+    return val == lines.Length - 1 ? 0 : val + 1;
+};
+var wrappedPrev = (long val) =>
+{
+    return val == 0 ? lines.Length - 1 : val - 1;
+};
+
+for (int i = 0; i < lines.Length; i++)
+{
+    string[] nums = lines[i].Split(',');
+    long x = long.Parse(nums[0]);
+    long y = long.Parse(nums[1]);
+    if (!xToY.ContainsKey(x))
+    {
+        xToY[x] = new() { y };
+    }
+    else
+    {
+        xToY[x].Add(y);
+    }
+    if (!yToX.ContainsKey(y))
+    {
+        yToX[y] = new() { x };
+    }
+    else
+    {
+        yToX[y].Add(x);
+    }
+    corners[i] = (x, y);
+    cornerMap[(x, y)] = i;
+}
+
+for (int i = 0; i < lines.Length; i++)
+{
+    var prevCorner = corners[wrappedPrev(i)];
+    var corner = corners[i];
+    if (prevCorner.x == corner.x)
+    {
+        var minY = Math.Min(prevCorner.y, corner.y);
+        var maxY = Math.Max(prevCorner.y, corner.y);
+
+        for (long j = minY; j <= maxY; j++)
+        {
+            if (!yToXEdges.ContainsKey(j))
+            {
+                yToXEdges[j] = new();
+            }
+            yToXEdges[j].Add(corner.x);
+        }
+    }
+    else
+    {
+        var minY = Math.Min(prevCorner.y, corner.y);
+        var maxY = Math.Max(prevCorner.y, corner.y);
+
+        var minX = Math.Min(prevCorner.x, corner.x);
+        var maxX = Math.Max(prevCorner.x, corner.x);
+
+        for (long j = minX; j <= maxX; j++)
+        {
+            if (!xToYEdges.ContainsKey(j))
+            {
+                xToYEdges[j] = new();
+            }
+            xToYEdges[j].Add(corner.y);
+        }
+    }
+}
+
+var isValid2 = ((long x, long y) c1, (long x, long y) c2) =>
+{
+    var minX = Math.Min(c1.x, c2.x);
+    var maxX = Math.Max(c1.x, c2.x);
+    var minY = Math.Min(c1.y, c2.y);
+    var maxY = Math.Max(c1.y, c2.y);
+    if (
+        xToYEdges
+            .Where(kvp =>
+                kvp.Key > minX && kvp.Key < maxX && kvp.Value.Where(y => y > minY && y < maxY).Any()
+            )
+            .Any()
+        || yToXEdges
+            .Where(kvp =>
+                kvp.Key > minY && kvp.Key < maxY && kvp.Value.Where(x => x > minX && x < maxX).Any()
+            )
+            .Any()
+    )
+    {
+        return false;
+    }
+    return true;
+};
+
+for (int i = 0; i < lines.Length - 1; i++)
+{
+    for (int j = i + 1; j < lines.Length; j++)
+    {
+        var corneri = corners[i];
+        var cornerj = corners[j];
+        long localArea =
+            (Math.Abs(corners[i].x - corners[j].x) + 1)
+            * (Math.Abs(corners[i].y - corners[j].y) + 1);
+
+        if (localArea > maxAreaPt1)
+        {
+            maxAreaPt1 = localArea;
+        }
+
+        if (localArea > maxAreaPt2 && isValid2(corners[i], corners[j]))
+        {
+            maxAreaPt2 = localArea;
+        }
+    }
+}
+
+Console.WriteLine(maxAreaPt1);
+Console.WriteLine(maxAreaPt2);
